47bb3ca9b6d8eb58fb85daafdb035378aa848c98
wwzyww <1390996371@qq.com>
2019-09-27 16:24:24 +0800

    init repo

---
+++ src/main/java/com/ww/springbootcommunity/SpringbootCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.ww.springbootcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCommunityApplication.class, args);
+    }
+
+}
